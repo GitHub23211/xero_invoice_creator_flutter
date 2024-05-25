265f8e791ef3a4093996480335e9a8f3da0f3637
Joshua Santos <joshua.santos1@students.mq.edu.au>
2024-05-25 20:43:42 +1000

    Change initial menu text

--- lib/providers/menu_text_provider.dart
+++ lib/providers/menu_text_provider.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class MenuTextProvider extends ChangeNotifier {
   String text;
-  MenuTextProvider({this.text = 'test'});
+  MenuTextProvider({this.text = 'Welcome!'});
 
   void changeText(String newText) {
     text = newText;
