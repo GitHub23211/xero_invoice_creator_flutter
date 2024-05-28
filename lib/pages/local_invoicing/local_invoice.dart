b12521c5fdda922cae39ec3a02949edc56c9714e
Joshua Santos <joshua.santos1@students.mq.edu.au>
2024-05-29 01:03:56 +1000

    Move invoice_form into local_invoice

--- lib/pages/local_invoicing/local_invoice.dart
+++ lib/pages/local_invoicing/local_invoice.dart
@@ -1,20 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:xero_app_flutter/global_components/navbar.dart';
-import 'package:xero_app_flutter/pages/local_invoicing/components/invoice_form.dart';
+import 'package:xero_app_flutter/pages/local_invoicing/components/entered_manifests.dart';
+import 'package:xero_app_flutter/pages/local_invoicing/components/form_buttons.dart';
+import 'package:xero_app_flutter/pages/local_invoicing/components/invoice_info_input.dart';
+import 'package:xero_app_flutter/pages/local_invoicing/components/store_num_form.dart';
+import 'package:xero_app_flutter/providers/data_provider.dart';
 
-class LocalInvoicing extends StatefulWidget {
-  const LocalInvoicing({super.key});
+class LocalInvoicing extends StatelessWidget {
+  LocalInvoicing({super.key});
 
-  @override
-  State<LocalInvoicing> createState() => _LocalInvoicingState();
-}
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
-class _LocalInvoicingState extends State<LocalInvoicing> {
+  void _onAdd() {
+    _formKey.currentState?.save();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: NavBar(title: 'Create Local Invoice'),
-      body: InvoiceForm(),
-    );
+    return Scaffold(
+        appBar: const NavBar(title: 'Create Local Invoice'),
+        body: Form(
+          key: _formKey,
+          child: Consumer<DataProvider>(
+            builder: (context, invoice, child) {
+              return Column(
+                children: [
+                  InvoiceInfoForm(),
+                  StoreNumForm(onAdd: _onAdd),
+                  EnteredManifests(),
+                  FormButtons(),
+                ],
+              );
+            },
+          ),
+        ));
   }
 }
