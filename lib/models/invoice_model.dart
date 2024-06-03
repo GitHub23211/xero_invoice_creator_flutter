578a3547782f898393744e85e70541094c823edc
Joshua Santos <joshua.santos1@students.mq.edu.au>
2024-06-04 02:39:20 +1000

    Define invoice model

--- lib/models/invoice_model.dart
+++ lib/models/invoice_model.dart
@@ -1,23 +1,58 @@
-import 'dart:io';
-import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:xero_app_flutter/models/csv_strategy.dart';
+import 'package:xero_app_flutter/models/add_item_strategy.dart';
+import 'package:xero_app_flutter/models/local_add_strategy.dart';
+import 'package:xero_app_flutter/models/save_strategy.dart';
 
 class InvoiceModel {
-  late List<List<dynamic>> _invoice;
+  late final Map<String, dynamic> _invoice;
+  late SaveStrategy _save;
+  late AddItemStrategy _strat;
 
-  InvoiceModel(List<dynamic> headers) {
-    _invoice = [headers];
+  /// _invoice Map structure per the Xero API POST Invoices specifications
+  InvoiceModel(
+    Map<String, dynamic> p,
+    Map<String, dynamic> f,
+  ) {
+    _save = CsvStrategy(f['HEADERS']);
+    _strat = LocalAddStrategy(p, f);
+    _invoice = {
+      'Type': 'ACCREC',
+      'Contact': f['CONTACT'],
+      'Date': null,
+      'DueDate': null,
+      'InvoiceNumber': null,
+      'Reference': null,
+      'CurrencyCode': 'AUD',
+      'Status': 'DRAFT',
+      'LineItems': <Map<String, dynamic>>[],
+      'ManNums': <String>{},
+    };
   }
 
-  void addItemLine(List<dynamic> item) {
-    _invoice.add(item);
+  List<Map<String, dynamic>> getItems() => _invoice['LineItems'];
+
+  String getInfo() =>
+      'Invoice Number - ${_invoice['InvoiceNumber']}, Date - ${_invoice['Date']}, Due Date - ${_invoice['DueDate']}';
+
+  void updateManNums(String num) => _invoice['ManNums'].add(num);
+
+  bool _manNumExists(String num) => _invoice['ManNums'].contains(num);
+
+  void setInvoiceInfo(String date, String due, String num) {
+    _invoice['Date'] = date;
+    _invoice['DueDate'] = due;
+    _invoice['InvoiceNumber'] = num;
+    debugPrint(_invoice.toString());
   }
 
-  Future<void> saveToCSV() async {
-    const String filename = './test.csv';
-    await File(filename).writeAsString(_convertToCsv());
+  void addLineItem(Map<String, dynamic> data) {
+    if (!_manNumExists(data['manNum'])) {
+      _strat.add(data, getItems());
+    }
   }
 
-  String _convertToCsv() {
-    return _invoice.join('\n').replaceAll(RegExp(r'\[|\]'), '');
+  void save() {
+    _save.save(_invoice);
   }
 }
