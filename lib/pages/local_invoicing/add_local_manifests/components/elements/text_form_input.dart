2f03de5f2f1c7d405b0bd72754c1d2f13f457687
Joshua Santos <joshua.santos1@students.mq.edu.au>
2024-06-06 00:49:44 +1000

    Refactor store_num_input, add validation to textFormField

--- lib/pages/local_invoicing/add_local_manifests/components/elements/text_form_input.dart
+++ lib/pages/local_invoicing/add_local_manifests/components/elements/text_form_input.dart
@@ -1,35 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class TextFormInput extends StatelessWidget {
   final String? labelText;
+  final int? maxLength;
+  final bool readOnly;
+  final List<TextInputFormatter>? inputFormatters;
+  final TextEditingController? controller;
+  final String? Function(String?)? validator;
   final Function(String?)? onSaved;
   final Function()? onTap;
-  final TextEditingController? controller;
-  final bool readOnly;
 
   const TextFormInput({
     super.key,
-    this.onSaved,
-    this.onTap,
-    this.controller,
     this.labelText,
     this.readOnly = false,
+    this.inputFormatters,
+    this.controller,
+    this.validator,
+    this.onSaved,
+    this.onTap,
+    this.maxLength,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        margin: const EdgeInsets.all(10.0),
-        child: TextFormField(
-          decoration: InputDecoration(
-            border: const OutlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(10))),
-            labelText: labelText,
-          ),
-          controller: controller,
-          onSaved: onSaved,
-          onTap: onTap,
-          readOnly: readOnly,
-        ));
+      margin: const EdgeInsets.all(10.0),
+      child: TextFormField(
+        decoration: InputDecoration(
+          border: const OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10))),
+          labelText: labelText,
+        ),
+        controller: controller,
+        onSaved: onSaved,
+        onTap: onTap,
+        readOnly: readOnly,
+        validator: validator,
+        inputFormatters: inputFormatters,
+        maxLength: maxLength,
+      ),
+    );
   }
 }
--- lib/pages/local_invoicing/add_local_manifests/components/store_num_form.dart
+++ lib/pages/local_invoicing/add_local_manifests/components/store_num_form.dart
@@ -1,49 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:xero_app_flutter/pages/local_invoicing/add_local_manifests/components/elements/text_form_input.dart';
 
 class StoreNumForm extends StatelessWidget {
   final Function() onSubmit;
   final Function(String? s) onSaved;
+  final int numStoreInputs = 3;
+  final RegExp regExp = RegExp(r'[^\d]');
 
-  const StoreNumForm({
+  StoreNumForm({
     super.key,
     required this.onSubmit,
     required this.onSaved,
   });
 
+  String? _validator(String? input) {
+    if (input == null) return null;
+    RegExpMatch? match = regExp.firstMatch(input);
+    if (match != null) return 'Nubmers only';
+    return null;
+  }
+
+  FilteringTextInputFormatter _filterInputFormatter() =>
+      FilteringTextInputFormatter.deny(regExp);
+
+  List<Widget> _createStoreNumInputs(double mQW) =>
+      List<Widget>.generate(numStoreInputs, (_) => _storeNumInput(mQW))
+        ..add(_addManifestButton());
+
+  Widget _storeNumInput(double mQW) => SizedBox(
+        width: mQW,
+        child: TextFormInput(
+          labelText: 'Store',
+          maxLength: 3,
+          onSaved: onSaved,
+          validator: _validator,
+          inputFormatters: [_filterInputFormatter()],
+        ),
+      );
+
+  Widget _addManifestButton() => ExcludeFocus(
+        child: ElevatedButton(
+          onPressed: onSubmit,
+          child: const Text('Add Manifest'),
+        ),
+      );
+
   @override
   Widget build(BuildContext context) {
     double mQueryWidth = MediaQuery.of(context).size.width * 1;
+
     return Container(
       margin: const EdgeInsets.all(10.0),
       child: Column(
-        children: [
-          SizedBox(
-            width: mQueryWidth,
-            child: TextFormInput(
-              labelText: 'Store',
-              onSaved: onSaved,
-            ),
-          ),
-          SizedBox(
-              width: mQueryWidth,
-              child: TextFormInput(
-                labelText: 'Store',
-                onSaved: onSaved,
-              )),
-          SizedBox(
-              width: mQueryWidth,
-              child: TextFormInput(
-                labelText: 'Store',
-                onSaved: onSaved,
-              ) //data.addManifest(s)),
-              ),
-          ExcludeFocus(
-              child: ElevatedButton(
-            onPressed: onSubmit,
-            child: const Text('Add Manifest'),
-          )),
-        ],
+        children: _createStoreNumInputs(mQueryWidth),
       ),
     );
   }
