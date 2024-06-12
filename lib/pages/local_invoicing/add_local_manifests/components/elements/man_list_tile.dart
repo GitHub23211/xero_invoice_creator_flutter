ad15938604df54dbd093f39758c19b5eb14976fc
Joshua Santos <joshua.santos1@students.mq.edu.au>
2024-06-13 01:35:25 +1000

    Add divider between entered manifests and conditionally render delete button

--- lib/pages/local_invoicing/add_local_manifests/components/elements/man_list_tile.dart
+++ lib/pages/local_invoicing/add_local_manifests/components/elements/man_list_tile.dart
@@ -9,21 +9,32 @@ class ManListTile extends StatelessWidget {
     this.onTap,
   });
 
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      titleAlignment: ListTileTitleAlignment.center,
-      title: Text('${itemLine['Description']}'),
-      subtitle: Text('\$${itemLine['UnitAmount']}'),
-      trailing: InkWell(
-        onTap: onTap,
-        hoverColor: Colors.black12,
-        borderRadius: BorderRadius.circular(10.0),
-        child: const Icon(
+  Widget _renderDeleteButton() => itemLine['ItemCode'] == ('DROP-RATE')
+      ? const SizedBox(
+          width: 0.0,
+        )
+      : const Icon(
           Icons.cancel_outlined,
           color: Colors.red,
-        ),
+        );
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      ListTile(
+        titleAlignment: ListTileTitleAlignment.center,
+        title: Text('${itemLine['Description']}'),
+        subtitle: Text('\$${itemLine['UnitAmount']}'),
+        trailing: InkWell(
+            onTap: onTap,
+            hoverColor: Colors.black12,
+            borderRadius: BorderRadius.circular(10.0),
+            child: _renderDeleteButton()),
       ),
-    );
+      const Divider(
+        height: 5,
+        thickness: 2,
+      ),
+    ]);
   }
 }
