23035d19e74464d838cef22e720ec671b09024fb
Joshua Santos <joshua.santos1@students.mq.edu.au>
2024-06-16 02:15:44 +1000

    Add style to leading text and placeholder trailing options.

--- lib/pages/store_pricings/components/elements/store_info_tile.dart
+++ lib/pages/store_pricings/components/elements/store_info_tile.dart
@@ -7,14 +7,37 @@ class StoreInfoTile extends StatelessWidget {
     required this.storeInfo,
   });
 
+  Widget _tileOptions() => SizedBox(
+        width: 100.0,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.edit),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.close),
+            ),
+          ],
+        ),
+      );
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      leading: Text(storeInfo[2]),
+      leading: Text(
+        storeInfo[2],
+        style: const TextStyle(
+          fontSize: 16.0,
+        ),
+      ),
       title: Text(storeInfo[0]),
       subtitle: Text(
         '\$${storeInfo[1]}',
       ),
+      trailing: _tileOptions(),
     );
   }
 }
